243175651730d47be4209dcdf5ff240225d8b684
avanim <axm1205@case.edu>
2022-12-12 17:39:43 -0500

    created GET and POST api routes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require("path");
 const PORT = 3001;
 const app = express();
 
+// Middleware for parsing JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // HTML routes
@@ -13,15 +16,40 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// Wildcard (fallback route)
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
 // API routes
 // Read db.json and return all saved notes as JSON
 app.get("/api/notes", (req, res) => {
-    return res.json(db);
+    console.info(`${req.method} /api/notes`);
+    res.status(200).json(db);
+});
+
+// POST request to save a new note
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add a new note`);
+
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: Math.random()   // replace with npm package?
+        };
+
+        const response = {
+            status: "success",
+            body: newNote
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json("Error in posting note");
+    }
+});
+
+// Wildcard (fallback route)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => 
